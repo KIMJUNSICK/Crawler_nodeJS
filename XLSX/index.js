9f8a9a1c9f6699cd2c3b0d47820243a40270abc0
Kim Jun Sik <jinaganda5@gmail.com>
2019-09-18 10:05:57 +0900

    First Crawling with axios & cheerio

--- XLSX/index.js
+++ XLSX/index.js
@@ -1,4 +1,6 @@
 import xlsx from "xlsx";
+import axios from "axios";
+import cheerio from "cheerio";
 
 const xlFile = xlsx.readFile("./xlsx/data.xlsx");
 const sheets = xlFile.Sheets;
@@ -8,14 +10,30 @@ const records = xlsx.utils.sheet_to_json(movieChart); // xlsx to json
 
 // xl file => json(key:value)
 
-records.forEach((obj, index) => {
-  console.log(index, obj.제목, obj.링크);
+records.forEach((record, index) => {
+  console.log(index, record.제목, record.링크);
 });
 
 // Array.entries  => two-dimensional Array
-for (const [index, obj] of records.entries()) {
-  console.log(index, obj.제목, obj.링크);
+for (const [index, record] of records.entries()) {
+  console.log(index, record.제목, record.링크);
 }
+
+const crawler = async () => {
+  await Promise.all(
+    records.map(async record => {
+      const response = await axios.get(record.링크);
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html); // html tag에 접근 가능하게.
+        const ntzScore = $(".score.score_left .star_score").text();
+        console.log(record.제목, "score:", ntzScore.trim());
+      }
+    })
+  );
+};
+
+crawler();
 
 // difference between 'forEach' and 'for of'
 // appear when async occurs
