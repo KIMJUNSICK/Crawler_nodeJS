6e94b2ab67f0fe8794649f1160869140117f37a4
Kim Jun Sik <jinaganda5@gmail.com>
2019-09-19 12:29:38 +0900

    option: headless & Promise.all for performance

--- Puppeteer/index.js
+++ Puppeteer/index.js
@@ -10,20 +10,42 @@ console.log(records); // parse => two-dimension Array
 // all works is asynchronous
 // so async/await is essential
 const crawler = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  const page2 = await browser.newPage();
-  const page3 = await browser.newPage();
-  await page.goto("https://zerocho.com");
-  await page.waitFor(3000);
+  const browser = await puppeteer.launch({
+    headless: process.env.NODE_ENV === "production"
+  }); // default of headless opt is 'true' => no display
+
+  // const page = await browser.newPage();
+  // const page2 = await browser.newPage();
+  // const page3 = await browser.newPage();
+
+  const [page, page2, page3] = await Promise.all([
+    browser.newPage(),
+    browser.newPage(),
+    browser.newPage()
+  ]); // 1:1 matching
+
+  // await page.goto("https://zerocho.com"),
+  // await page2.goto("https://www.youtube.com/?gl=KR"),
+  // await page3.goto("https://academy.nomadcoders.co/")
+  //=> not efficient
+
+  await Promise.all([
+    page.goto("https://zerocho.com"),
+    page2.goto("https://www.youtube.com/?gl=KR"),
+    page3.goto("https://academy.nomadcoders.co/")
+  ]); // more efficient // modern javascript is awesome!
+
+  await Promise.all([
+    page.waitFor(3000),
+    page2.waitFor(3000),
+    page3.waitFor(3000)
+  ]);
+
   await page.close();
-  await page2.goto("https://www.youtube.com/?gl=KR");
-  await page2.waitFor(3000);
   await page2.close();
-  await page3.goto("https://academy.nomadcoders.co/");
-  await page3.waitFor(3000);
   await page3.close();
   await browser.close();
+  // Promise.all is more fast than awiat's'
 };
 
 crawler();
